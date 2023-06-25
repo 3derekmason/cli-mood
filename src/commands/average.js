3512399806baf1753486595838428996fc6c0eea
3derekmason <3derekmason@gmail.com>
2023-06-24 20:32:54 -0600

    style: color based on average

--- src/commands/average.js
+++ src/commands/average.js
@@ -12,7 +12,22 @@ export function calculateAverage(range) {
   const average = calculateAverageValue(filteredItems);
 
   console.log(chalk.blue("- - - - - - - - - - - - - - - - - - - -"));
-  console.log(`Average for ${range ? `(${range})` : "(all time)"}:`);
-  console.log(chalk.bold(`\n     ${average.toFixed(2)}`));
+  console.log(`${range ? `(${range})` : "(all time)"} average:`);
+
+  if (average >= 0 && average < 2) {
+    console.log(chalk.hex("#ef4444").bold(`\n     ${average.toFixed(2)}`));
+  }
+  if (average >= 2 && average < 4) {
+    console.log(chalk.hex("#f97316").bold(`\n     ${average.toFixed(2)}`));
+  }
+  if (average >= 4 && average < 6) {
+    console.log(chalk.hex("#eab308").bold(`\n     ${average.toFixed(2)}`));
+  }
+  if (average >= 6 && average < 8) {
+    console.log(chalk.hex("#84cc16").bold(`\n     ${average.toFixed(2)}`));
+  }
+  if (average >= 8) {
+    console.log(chalk.hex("#22c55e").bold(`\n     ${average.toFixed(2)}`));
+  }
   console.log(chalk.blue("- - - - - - - - - - - - - - - - - - - -"));
 }
